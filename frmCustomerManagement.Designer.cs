59e54d171089c4f34555ad9a5e3e93b64b752404
Phạm Tiến Dũng <dungpham280306nd@gmail.com>
2025-08-21 21:45:53 +0700

    Update frmCustomerManagement.Designer.cs

--- frmCustomerManagement.Designer.cs
+++ frmCustomerManagement.Designer.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows.Forms;
+using System.Windows.Forms;
 
 namespace StoreXManagerApp
 {
@@ -43,33 +43,33 @@ namespace StoreXManagerApp
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(505, 11);
+            this.btnSearch.Location = new System.Drawing.Point(577, 12);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 24);
+            this.btnSearch.Size = new System.Drawing.Size(86, 26);
             this.btnSearch.TabIndex = 1;
-            this.btnSearch.Text = "Tìm";
+            this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(127, 12);
+            this.txtSearch.Location = new System.Drawing.Point(145, 13);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(372, 23);
+            this.txtSearch.Size = new System.Drawing.Size(425, 22);
             this.txtSearch.TabIndex = 0;
             // 
             // lblSearch
             // 
             this.lblSearch.AutoSize = true;
-            this.lblSearch.Location = new System.Drawing.Point(12, 15);
+            this.lblSearch.Location = new System.Drawing.Point(75, 16);
             this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(109, 15);
+            this.lblSearch.Size = new System.Drawing.Size(33, 16);
             this.lblSearch.TabIndex = 4;
-            this.lblSearch.Text = "Tìm kiếm (theo tên):";
+            this.lblSearch.Text = "Find";
             // 
             // grpCustomerDetails
             // 
-            this.grpCustomerDetails.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            this.grpCustomerDetails.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.grpCustomerDetails.Controls.Add(this.btnDelete);
             this.grpCustomerDetails.Controls.Add(this.btnSave);
@@ -84,157 +84,161 @@ namespace StoreXManagerApp
             this.grpCustomerDetails.Controls.Add(this.lblCustomerName);
             this.grpCustomerDetails.Controls.Add(this.txtCustomerID);
             this.grpCustomerDetails.Controls.Add(this.lblCustomerID);
-            this.grpCustomerDetails.Location = new System.Drawing.Point(586, 12);
+            this.grpCustomerDetails.Location = new System.Drawing.Point(670, 13);
             this.grpCustomerDetails.Name = "grpCustomerDetails";
-            this.grpCustomerDetails.Size = new System.Drawing.Size(314, 426);
+            this.grpCustomerDetails.Size = new System.Drawing.Size(359, 454);
             this.grpCustomerDetails.TabIndex = 3;
             this.grpCustomerDetails.TabStop = false;
-            this.grpCustomerDetails.Text = "Thông tin khách hàng";
+            this.grpCustomerDetails.Text = "Customer information";
             // 
             // btnDelete
             // 
             this.btnDelete.BackColor = System.Drawing.Color.LightCoral;
-            this.btnDelete.Location = new System.Drawing.Point(212, 240);
+            this.btnDelete.Location = new System.Drawing.Point(242, 256);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(96, 33);
+            this.btnDelete.Size = new System.Drawing.Size(110, 35);
             this.btnDelete.TabIndex = 7;
-            this.btnDelete.Text = "Xóa";
+            this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = false;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnSave
             // 
             this.btnSave.BackColor = System.Drawing.Color.LightGreen;
-            this.btnSave.Location = new System.Drawing.Point(110, 240);
+            this.btnSave.Location = new System.Drawing.Point(126, 256);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(96, 33);
+            this.btnSave.Size = new System.Drawing.Size(110, 35);
             this.btnSave.TabIndex = 6;
-            this.btnSave.Text = "Lưu Thay Đổi";
+            this.btnSave.Text = "Save Changes";
             this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnAddNew
             // 
-            this.btnAddNew.Location = new System.Drawing.Point(8, 240);
+            this.btnAddNew.Location = new System.Drawing.Point(9, 256);
             this.btnAddNew.Name = "btnAddNew";
-            this.btnAddNew.Size = new System.Drawing.Size(96, 33);
+            this.btnAddNew.Size = new System.Drawing.Size(110, 35);
             this.btnAddNew.TabIndex = 5;
-            this.btnAddNew.Text = "Thêm Mới";
+            this.btnAddNew.Text = "Add";
             this.btnAddNew.UseVisualStyleBackColor = true;
             this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(109, 189);
+            this.txtEmail.Location = new System.Drawing.Point(126, 205);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(199, 23);
+            this.txtEmail.Size = new System.Drawing.Size(227, 22);
             this.txtEmail.TabIndex = 4;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(8, 192);
+            this.lblEmail.Location = new System.Drawing.Point(9, 205);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(39, 15);
+            this.lblEmail.Size = new System.Drawing.Size(44, 16);
             this.lblEmail.TabIndex = 8;
             this.lblEmail.Text = "Email:";
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(109, 149);
+            this.txtPhone.Location = new System.Drawing.Point(125, 159);
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(199, 23);
+            this.txtPhone.Size = new System.Drawing.Size(227, 22);
             this.txtPhone.TabIndex = 3;
             // 
             // lblPhone
             // 
             this.lblPhone.AutoSize = true;
-            this.lblPhone.Location = new System.Drawing.Point(8, 152);
+            this.lblPhone.Location = new System.Drawing.Point(9, 162);
             this.lblPhone.Name = "lblPhone";
-            this.lblPhone.Size = new System.Drawing.Size(82, 15);
+            this.lblPhone.Size = new System.Drawing.Size(46, 16);
             this.lblPhone.TabIndex = 6;
-            this.lblPhone.Text = "Số điện thoại:";
+            this.lblPhone.Text = "Phone";
             // 
             // txtAddress
             // 
-            this.txtAddress.Location = new System.Drawing.Point(109, 109);
+            this.txtAddress.Location = new System.Drawing.Point(125, 116);
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(199, 23);
+            this.txtAddress.Size = new System.Drawing.Size(227, 22);
             this.txtAddress.TabIndex = 2;
+            this.txtAddress.TextChanged += new System.EventHandler(this.txtAddress_TextChanged);
             // 
             // lblAddress
             // 
             this.lblAddress.AutoSize = true;
-            this.lblAddress.Location = new System.Drawing.Point(8, 112);
+            this.lblAddress.Location = new System.Drawing.Point(9, 119);
             this.lblAddress.Name = "lblAddress";
-            this.lblAddress.Size = new System.Drawing.Size(46, 15);
+            this.lblAddress.Size = new System.Drawing.Size(61, 16);
             this.lblAddress.TabIndex = 4;
-            this.lblAddress.Text = "Địa chỉ:";
+            this.lblAddress.Text = "Address:";
             // 
             // txtCustomerName
             // 
-            this.txtCustomerName.Location = new System.Drawing.Point(109, 69);
+            this.txtCustomerName.Location = new System.Drawing.Point(125, 74);
             this.txtCustomerName.Name = "txtCustomerName";
-            this.txtCustomerName.Size = new System.Drawing.Size(199, 23);
+            this.txtCustomerName.Size = new System.Drawing.Size(227, 22);
             this.txtCustomerName.TabIndex = 1;
             // 
             // lblCustomerName
             // 
             this.lblCustomerName.AutoSize = true;
-            this.lblCustomerName.Location = new System.Drawing.Point(8, 72);
+            this.lblCustomerName.Location = new System.Drawing.Point(9, 77);
             this.lblCustomerName.Name = "lblCustomerName";
-            this.lblCustomerName.Size = new System.Drawing.Size(95, 15);
+            this.lblCustomerName.Size = new System.Drawing.Size(104, 16);
             this.lblCustomerName.TabIndex = 2;
-            this.lblCustomerName.Text = "Tên khách hàng:";
+            this.lblCustomerName.Text = "Customer name:";
             // 
             // txtCustomerID
             // 
-            this.txtCustomerID.Location = new System.Drawing.Point(109, 29);
+            this.txtCustomerID.Location = new System.Drawing.Point(125, 31);
             this.txtCustomerID.Name = "txtCustomerID";
             this.txtCustomerID.ReadOnly = true;
-            this.txtCustomerID.Size = new System.Drawing.Size(199, 23);
+            this.txtCustomerID.Size = new System.Drawing.Size(227, 22);
             this.txtCustomerID.TabIndex = 0;
+            this.txtCustomerID.TextChanged += new System.EventHandler(this.txtCustomerID_TextChanged);
             // 
             // lblCustomerID
             // 
             this.lblCustomerID.AutoSize = true;
-            this.lblCustomerID.Location = new System.Drawing.Point(8, 32);
+            this.lblCustomerID.Location = new System.Drawing.Point(9, 34);
             this.lblCustomerID.Name = "lblCustomerID";
-            this.lblCustomerID.Size = new System.Drawing.Size(44, 15);
+            this.lblCustomerID.Size = new System.Drawing.Size(103, 16);
             this.lblCustomerID.TabIndex = 0;
-            this.lblCustomerID.Text = "Mã KH:";
+            this.lblCustomerID.Text = "Customer Code:";
             // 
             // dgvCustomers
             // 
             this.dgvCustomers.AllowUserToAddRows = false;
             this.dgvCustomers.AllowUserToDeleteRows = false;
-            this.dgvCustomers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.dgvCustomers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvCustomers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCustomers.Location = new System.Drawing.Point(12, 41);
+            this.dgvCustomers.Location = new System.Drawing.Point(14, 44);
             this.dgvCustomers.MultiSelect = false;
             this.dgvCustomers.Name = "dgvCustomers";
             this.dgvCustomers.ReadOnly = true;
+            this.dgvCustomers.RowHeadersWidth = 51;
             this.dgvCustomers.RowTemplate.Height = 25;
             this.dgvCustomers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvCustomers.Size = new System.Drawing.Size(568, 397);
+            this.dgvCustomers.Size = new System.Drawing.Size(649, 423);
             this.dgvCustomers.TabIndex = 2;
             this.dgvCustomers.SelectionChanged += new System.EventHandler(this.dgvCustomers_SelectionChanged);
             // 
             // frmCustomerManagement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(912, 450);
+            this.ClientSize = new System.Drawing.Size(1042, 480);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.lblSearch);
             this.Controls.Add(this.grpCustomerDetails);
             this.Controls.Add(this.dgvCustomers);
             this.Name = "frmCustomerManagement";
-            this.Text = "Quản lý Khách hàng";
+            this.Text = "Customer Management";
             this.Load += new System.EventHandler(this.frmCustomerManagement_Load);
             this.grpCustomerDetails.ResumeLayout(false);
             this.grpCustomerDetails.PerformLayout();
