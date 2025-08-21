64ed99b010de5f6c7471b27d702ba4d6298bd64a
Phạm Tiến Dũng <dungpham280306nd@gmail.com>
2025-08-21 21:49:27 +0700

    Update frmLogin.Designer.cs

--- frmLogin.Designer.cs
+++ frmLogin.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Dán toàn bộ code này vào file frmLogin.Designer.cs
+// Dán toàn bộ code này vào file frmLogin.Designer.cs
 
 using System.Windows.Forms; // Chỉ cần dòng using này là đủ
 
@@ -32,64 +32,67 @@ namespace StoreXManagerApp
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(30, 33);
+            this.lblUsername.Location = new System.Drawing.Point(34, 35);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(90, 15);
+            this.lblUsername.Size = new System.Drawing.Size(76, 16);
             this.lblUsername.TabIndex = 0;
-            this.lblUsername.Text = "Tên đăng nhập:";
+            this.lblUsername.Text = "UserName:";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(126, 30);
+            this.txtUsername.Location = new System.Drawing.Point(144, 32);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(217, 23);
+            this.txtUsername.Size = new System.Drawing.Size(247, 22);
             this.txtUsername.TabIndex = 0;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(30, 75);
+            this.lblPassword.Location = new System.Drawing.Point(34, 80);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(60, 15);
+            this.lblPassword.Size = new System.Drawing.Size(67, 16);
             this.lblPassword.TabIndex = 2;
-            this.lblPassword.Text = "Mật khẩu:";
+            this.lblPassword.Text = "Password";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(126, 72);
+            this.txtPassword.Location = new System.Drawing.Point(144, 77);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(217, 23);
+            this.txtPassword.Size = new System.Drawing.Size(247, 22);
             this.txtPassword.TabIndex = 1;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
             // 
             // btnLogin
             // 
-            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnLogin.Location = new System.Drawing.Point(126, 118);
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.Location = new System.Drawing.Point(144, 126);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(105, 33);
+            this.btnLogin.Size = new System.Drawing.Size(120, 35);
             this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(237, 118);
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(271, 126);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(106, 33);
+            this.btnExit.Size = new System.Drawing.Size(121, 35);
             this.btnExit.TabIndex = 3;
-            this.btnExit.Text = "Thoát";
+            this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // frmLogin
             // 
             this.AcceptButton = this.btnLogin;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(384, 181);
+            this.ClientSize = new System.Drawing.Size(439, 193);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtPassword);
@@ -104,6 +107,7 @@ namespace StoreXManagerApp
             this.Text = "Đăng nhập";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
